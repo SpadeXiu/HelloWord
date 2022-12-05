0d74017831a214a49c25d4b0db5444115f5820bd
onlyxiu <chenxiusheng0@gmail.com>
2022-12-05 21:45:28 +0800

    实现sort函数

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 void sort(int arr[],int size)
 {
+  int tmp = 0;
+  for(int i = 0;i < size -1 ;i++)
+  {
+	for(int j = 0; j < size -i-1;j++)
+	{
+		if(arr[j] > arr[j+1])
+		{
+			tmp = arr[j];
+			arr[j] = arr[j+1];
+			arr[j+1] = tmp;
+		}
 
+	}
+  }
 }
 
 int main()
